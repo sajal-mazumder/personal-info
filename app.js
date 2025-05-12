80c4b689bce461d86747e0f28f8791ea8c9c65af
sajal-mazumder <sajalmazumder47@gmail.com>
2025-05-12 23:44:15 +0600

    some update

--- app.js
+++ app.js
@@ -18,11 +18,39 @@ window.onload = () => {
         <p>Mobile Number : ${savedData.mobileNo}</p> 
         `
     }
-    else{
-         output.innerHTML = `<p> Data not found </p>`
-    }
+    // else{
+    //      output.innerHTML = `<p> Data not found </p>`
+    // } // no need this line 
 
-    form.addEventListener("submit", ()=> {
-        alert("hello")
+    form.addEventListener("submit", (event)=> {
+        // alert("hello")
+
+        event.preventDefault() // to stop reload only usage
+        // console.log(event);
+
+        const id = document.getElementById("id").value;
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;       ;
+        const mobile = document.getElementById("mobileNo").value;
+
+        // console.log(name.value);
+
+        const formData = {
+            id : id, 
+            name : name,
+            email : email,
+            mobileNo : mobile 
+        }
+
+        localStorage.setItem("formData", JSON.stringify(formData)) // const formData and getItem formData should be same spelling.
+
+        output.innerHTML = `
+        <h3>ID : ${id}</h3>
+        <h4>Name : ${name}</h4>
+        <p>Email : ${email}</p>
+        <p>Mobile Number : ${mobile}</p>  
+        ` // these code for instant load of data
+        
+        form.reset()
     })
 }
